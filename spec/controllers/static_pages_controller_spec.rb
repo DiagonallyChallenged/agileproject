225a188dd16e11430ff5b63b95879d263df2e813
Megan Faley <megan.faley@gmail.com>
2019-09-30 00:12:23 +0000

    Updated so only one root path

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe GamesController, type: :controller do
-	describe "games#index action" do
+RSpec.describe StaticPagesController, type: :controller do
+	describe "static_pages#index action" do
 		it "should successfully show the page" do
 			get :index
 			expect(response).to have_http_status(:success)
