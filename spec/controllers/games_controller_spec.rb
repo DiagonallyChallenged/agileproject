42ceb2f6187080b7371566b9bcab8528e37f78b4
Madeline Young <madelinejy@gmail.com>
2019-10-01 14:58:26 +0000

    Generated Game Model and Games Controller

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe GamesController, type: :controller do
+
+  describe "games#index action" do
+    it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe "games#new action" do
+    it 'should successfully show the new form' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+end
