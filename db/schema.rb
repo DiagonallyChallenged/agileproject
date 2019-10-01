1d4f00ed3252b53813b91ca7c9b3bf4c7894a106
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-01 00:57:11 +0000

    ran rake db:migrate to get the app working again

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2019_09_28_160420) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
