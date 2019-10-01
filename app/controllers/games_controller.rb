a534fe5ed8fd7691f8bc0862402e5ef8475a17d2
Madeline Young <madelinejy@gmail.com>
2019-10-01 15:22:52 +0000

    Test Successful on new action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,12 @@
 class GamesController < ApplicationController
+  # before_action :authenticate_user!, only: [:new, :create]
+
+
+  def new
+    @game = Game.new
+  end
+
   def index
 
   end
-
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
-  describe "games#index action" do
-    it "should successfully show the page" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "static_pages#index action" do
+  #   it "should successfully show the page" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 
   describe "games#new action" do
