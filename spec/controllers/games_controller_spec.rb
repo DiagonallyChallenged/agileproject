c56f73d3b7baa7c2f7ae6d24fc8b799750a0c567
Madeline Young <madelinejy@gmail.com>
2019-10-02 21:27:18 +0000

    Rubocop passed

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GamesController, type: :controller do
   # end
 
   describe 'games#new action' do
-    it "should require users to be logged in" do
+    it 'should require users to be logged in' do
       get :new
       expect(response).to redirect_to new_user_session_path
     end
@@ -25,8 +25,8 @@ RSpec.describe GamesController, type: :controller do
   end
 
   describe 'games#create action' do
-    it "should require users to be logged in" do
-      post :create, params: { game: { message: "Hello" } }
+    it 'should require users to be logged in' do
+      post :create, params: { game: { message: 'Hello' } }
       expect(response).to redirect_to new_user_session_path
     end
     # code can be used for validation
@@ -48,24 +48,22 @@ RSpec.describe GamesController, type: :controller do
       user = FactoryBot.create(:user)
       sign_in user
 
-      game_count = Game.count
+      # game_count = Game.count
       post :create, params: { game: { name: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Game.count).to eq Game.count
     end
   end
 
   describe 'game#show action' do
+    it 'should successfully show the page if the game is found' do
+      game = FactoryBot.create(:game)
+      get :show, params: { id: game.id }
+      expect(response).to have_http_status(:found)
+    end
 
-     it 'should successfully show the page if the game is found' do
-       game = FactoryBot.create(:game)
-       get :show, params: { id: game.id }
-       expect(response).to have_http_status(:found)
-     end
-
-     it 'should return a 302 error if the game is not found' do
-       get :show, params: { id: 'TACOCAT' }
-       expect(response).to have_http_status(:found)
-     end
-   end
+    it 'should return a 302 error if the game is not found' do
+      get :show, params: { id: 'TACOCAT' }
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
