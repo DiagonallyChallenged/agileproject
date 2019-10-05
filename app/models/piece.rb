2dfaa457f9cb156e5902ded4cbce6e3885598c2f
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-05 17:20:36 +0000

    Got inital pass at diagonals, still need to test....

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -15,10 +15,31 @@ class Piece < ApplicationRecord
       x_low, x_high = [x_current, x_destination].sort
       (x_low + 1).upto(x_high - 1) { |x| self.game.check_space(x, y_low) }
     elsif (x_current - x_destination).abs == (y_current - y_destination).abs #diagonal
-      # figure diagonal
-
-      
-
+      if x_current > x_destination && y_current > y_destination # down/left
+        while x_current > x_destination && y_current > y_destination
+          x_current -= 1
+          y_current -= 1
+          self.game.check_space(x_current, y_current)
+        end
+      elsif x_current > x_destination && y_current < y_destination # up/left
+        while x_current > x_destination && y_current < y_destination
+          x_current -= 1
+          y_current += 1
+          self.game.check_space(x_current, y_current)
+        end
+      elsif x_current < x_destination && y_current < y_destination # up/right
+        while x_current < x_destination && y_current < y_destination
+          x_current += 1
+          y_current += 1
+          self.game.check_space(x_current, y_current)
+        end
+      elsif x_current < x_destination && y_current > y_destination # down/right
+        while x_current < x_destination && y_current > y_destination
+          x_current += 1
+          y_current -= 1
+          self.game.check_space(x_current, y_current)
+        end
+      end
     else
       return 'Move not valid' # Invalid move. Change this to raise an error?
     end
