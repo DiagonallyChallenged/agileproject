87046d47db289ea25c28c7e5ae87ab9601042355
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-07 23:07:35 +0000

    Changed first if/else statementto a case

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,17 +4,33 @@ class Piece < ApplicationRecord
   alias_attribute :x, :x_location
   alias_attribute :y, :y_location
 
-  def obstructed?(x_destination, y_destination)
+  def movement_direction(x_destination, y_destination)
     x_current = x_location
     y_current = y_location
 
     if x_current == x_destination # horizontal
+      'horizontal'
+    elsif y_current == y_destination # vertical
+      'vertical'
+    elsif (x_current - x_destination).abs == (y_current - y_destination).abs # diagonal
+      'diagonal'
+    else
+      false
+    end
+  end
+
+  def obstructed?(x_destination, y_destination)
+    x_current = x_location
+    y_current = y_location
+
+    case movement_direction(x_destination, y_destination)
+    when 'horizontal'
       y_low, y_high = [y_current, y_destination].sort
       (y_low + 1).upto(y_high - 1) { |y| return true if game.space_occupied?(x_current, y) }
-    elsif y_current == y_destination # vertical
+    when 'vertical'
       x_low, x_high = [x_current, x_destination].sort
       (x_low + 1).upto(x_high - 1) { |x| return true if game.space_occupied?(x, y_current) }
-    elsif (x_current - x_destination).abs == (y_current - y_destination).abs # diagonal
+    when 'diagonal'
       if x_current > x_destination && y_current > y_destination # down/left
         while x_current > x_destination && y_current > y_destination
           x_current -= 1
