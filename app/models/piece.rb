13ddf7b0abe078e8f1c9b732e750c06b1518353d
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-07 17:35:15 +0000

    Made some rubocop fixes, still thinks method is too complex

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,7 +4,7 @@ class Piece < ApplicationRecord
   alias_attribute :x, :x_location
   alias_attribute :y, :y_location
 
-  def is_obstructed?(x_destination, y_destination)
+  def obstructed?(x_destination, y_destination)
     x_current = self.x_location
     y_current = self.y_location
 
@@ -12,40 +12,40 @@ class Piece < ApplicationRecord
 
     if x_current == x_destination # horizontal
       y_low, y_high = [y_current, y_destination].sort
-      (y_low + 1).upto(y_high - 1) { |y| obstructed = space_status.push(self.game.check_space(x_current, y)) }
+      (y_low + 1).upto(y_high - 1) { |y| space_status.push(game.check_space(x_current, y)) }
     elsif y_current == y_destination # vertical
       x_low, x_high = [x_current, x_destination].sort
-      (x_low + 1).upto(x_high - 1) { |x| obstructed = space_status.push(self.game.check_space(x, y_current)) }
-    elsif (x_current - x_destination).abs == (y_current - y_destination).abs #diagonal
+      (x_low + 1).upto(x_high - 1) { |x| space_status.push(game.check_space(x, y_current)) }
+    elsif (x_current - x_destination).abs == (y_current - y_destination).abs # diagonal
       if x_current > x_destination && y_current > y_destination # down/left
         while x_current > x_destination && y_current > y_destination
           x_current -= 1
           y_current -= 1
-          space_status.push(self.game.check_space(x_current, y_current))
+          space_status.push(game.check_space(x_current, y_current))
         end
       elsif x_current > x_destination && y_current < y_destination # up/left
         while x_current > x_destination && y_current < y_destination
           x_current -= 1
           y_current += 1
-          space_status.push(self.game.check_space(x_current, y_current))
+          space_status.push(game.check_space(x_current, y_current))
         end
       elsif x_current < x_destination && y_current < y_destination # up/right
         while x_current < x_destination && y_current < y_destination
           x_current += 1
           y_current += 1
-          space_status.push(self.game.check_space(x_current, y_current))
+          space_status.push(game.check_space(x_current, y_current))
         end
       elsif x_current < x_destination && y_current > y_destination # down/right
         while x_current < x_destination && y_current > y_destination
           x_current += 1
           y_current -= 1
-          space_status.push(obstructed = self.game.check_space(x_current, y_current))
+          space_status.push(game.check_space(x_current, y_current))
         end
       end
     else
       return 'Move not valid' # Invalid move. Change this to raise an error?
     end
 
-    return space_status.include?(true)
+    space_status.include?(true)
   end
 end
