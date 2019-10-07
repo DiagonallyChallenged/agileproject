d0ab54d13b063a7e4012a10bc1253ba349309b3a
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-10-07 21:33:17 +0000

    Added tests for both methods

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,4 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+
+  describe 'check_space method' do
+    it 'should return true if space has a piece' do
+
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece = FactoryBot.create(:piece, id: 1, x_location: 1, y_location: 1, game_id: game.id, user_id: user.id)
+
+      expect(game.check_space(1, 1)).to be true
+    end
+    
+    it 'should return false if space has no piece' do
+
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece = FactoryBot.create(:piece, id: 1, x_location: 1, y_location: 1, game_id: game.id, user_id: user.id)
+
+      expect(game.check_space(3, 3)).to be false
+    end
+
+  end
+
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,4 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
+
+  describe 'obstructed? method' do
+    it 'should return false if path is clear' do
+
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece1 = FactoryBot.create(:piece, id: 1, x_location: 1, y_location: 1, game_id: game.id, user_id: user.id)
+      piece2 = FactoryBot.create(:piece, id: 2, x_location: 3, y_location: 3, game_id: game.id, user_id: user.id)
+
+      expect(piece1.obstructed?(3, 1)).to be false
+
+    end
+
+    it 'should return true for horizontal obstructions' do
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece1 = FactoryBot.create(:piece, id: 1, x_location: 1, y_location: 1, game_id: game.id, user_id: user.id)
+      piece2 = FactoryBot.create(:piece, id: 2, x_location: 3, y_location: 1, game_id: game.id, user_id: user.id)
+
+      expect(piece1.obstructed?(5, 1)).to be true
+    end
+
+    it 'should return true for vertical obstructions' do
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece1 = FactoryBot.create(:piece, id: 1, x_location: 1, y_location: 5, game_id: game.id, user_id: user.id)
+      piece2 = FactoryBot.create(:piece, id: 2, x_location: 1, y_location: 3, game_id: game.id, user_id: user.id)
+
+      expect(piece1.obstructed?(1, 1)).to be true
+    end
+
+    it 'should return true for diagonal obstructions' do
+
+      game = FactoryBot.create(:game)
+      user = FactoryBot.create(:user)
+      piece1 = FactoryBot.create(:piece, id: 1, x_location: 2, y_location: 4, game_id: game.id, user_id: user.id)
+      piece2 = FactoryBot.create(:piece, id: 2, x_location: 3, y_location: 3, game_id: game.id, user_id: user.id)
+
+      expect(piece1.obstructed?(5, 1)).to be true
+
+    end
+  end
 end
