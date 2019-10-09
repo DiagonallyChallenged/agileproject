e170f02362a735e5cfb54cb2672c02ac11ec7190
Megan Faley <megan.faley@gmail.com>
2019-10-09 01:29:29 +0000

    refactored for rubocop

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,37 +9,30 @@ class Game < ApplicationRecord
   scope :available, -> { where('black_player_id IS NULL OR white_player_id IS NULL') }
 
   def populate_game
-  	create_pawns
-  	create_major_pieces
+    create_pawns
+    create_major_pieces
   end
 
   def create_pawns
-  	(1..8).each do |pawn|
-		Piece.create({x_location: pawn, y_location: 2, type: Pawn, game_id: self.id})
-		Piece.create({x_location: pawn, y_location: 7, type: Pawn, game_id: self.id})
-  	end
+    (1..8).each do |pawn|
+      Piece.create(x_location: pawn, y_location: 2, type: Pawn, game_id: id)
+      Piece.create(x_location: pawn, y_location: 7, type: Pawn, game_id: id)
+    end
   end
 
   def create_type(x_location, type)
-  	Piece.create({x_location: x_location, y_location: 1, type: type, game_id: self.id})
-  	Piece.create({x_location: x_location, y_location: 8, type: type, game_id: self.id})
+    Piece.create(x_location: x_location, y_location: 1, type: type, game_id: id)
+    Piece.create(x_location: x_location, y_location: 8, type: type, game_id: id)
   end
 
-
   def create_major_pieces
-  	(1..8).each do |count|
-  		case count
-  		when 1, 8
-  			create_type(count, Rook)
-  		when 2, 7
-  			create_type(count, Knight)
-  		when 3, 6
-  			create_type(count, Bishop)
-  		when 4
-  			create_type(count, Queen)
-  		when 5
-  			create_type(count, King)
-  		end		
-  	end
+    create_type(1, Rook)
+    create_type(8, Rook)
+    create_type(2, Knight)
+    create_type(7, Knight)
+    create_type(3, Bishop)
+    create_type(6, Bishop)
+    create_type(4, Queen)
+    create_type(5, King)
   end
 end
