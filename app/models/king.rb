3b162e59c03cca14b857171b24a4a34e4ea303de
Megan Faley <megan.faley@gmail.com>
2019-10-14 12:40:39 +0000

    WIP king move

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,10 +1,28 @@
 class King < Piece
 
-	def possible_moves
-		#responds with array x,y coordinates
+	def valid_move?(x:, y:)
+		location = {
+			x: x,
+			y: y,
+		}
+
+		( one_square_forward?(location) && empty_square?(location) ) || \
+		( diagonal_move?(location) && enemy_occupied_square?(location) )
 	end
 
-	def move
-		#change self location of piece
+	def one_square_forward?(location)
+		location[:y] == self.y + 1 &&
+		location[:x] == self.x 
 	end
+
+
+	def empty_square?(location)
+		Piece.find_by(location).nil?
+	end
+
+	def diagonal_move?(location)
+		( location[:y] == self.y + 1 ) &&
+		( location[:x].in?([ self.x + 1, self.x - 1]) )
+	end
+
 end
