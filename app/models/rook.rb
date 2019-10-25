7acfb343c3fabd4ef1fae6093e68672a79c2bd5a
emilykocurek <emilykocurek17@gmail.com>
2019-10-25 20:23:40 +0000

    Logic for valid rook refactor

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,12 +1,8 @@
 class Rook < Piece
  def valid_move?(x_des:, y_des:)
     return false if obstructed?(x, y, x_des, y_des)
-    if x == x_des 
-      return true
-    elsif y == y_des
-      return true
-    end
-      
+    
+    return true if (x == x_des || y == y_des) 
     
     false
   end
